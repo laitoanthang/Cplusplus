b721b1997bab1ab37047bd8c876f84eba58244b8
thang_dumbest <laitoanthang219@gmail.com>
2021-09-30 23:05:03 +0700

    Text To Binary

--- TH_KTLT/File/file.cpp
+++ TH_KTLT/File/file.cpp
@@ -1,9 +1,6 @@
 // ios::app--> "We cannot move the pointer. It will be only at end."
-
 // ios::ate--> "We can move the record pointer to any other place."
-
 // ios::binary --> to record the data in the binary mode
-
 // ios::nocreate
 // ios::noreplace
 // ios::trunc <- erase the file if it exists.
@@ -11,15 +8,16 @@
 #include <fstream>
 using namespace std;
 
-ofstream fout;
+
 
 void writing_file()
 {
+	ofstream fout;
 	char tentt[50];
 	cin.ignore();
 	cout << "Input your file\n";
 	cin.getline(tentt, 51);
-	fout.open(tentt);
+	fout.open(tentt, ios::out);
 	if (!fout.is_open())
 	{
 		cout << "Error, cannot open your file";
@@ -29,22 +27,58 @@ void writing_file()
 	{
 		fout << "Nay cau oi!!!\n";
 		fout << "To co dieu muon noi\n";
-		fout.close();
 		cout << "Successful process\n";
 	}
+	fout.close();
 }
 
 void writing_extra() 
 {
+	ofstream fout;
 	char tentt[50];
 	cout << "Input your file\n";
 	cin.ignore();
 	cin.getline(tentt, 51);
 	fout.open(tentt, ios::app);
+	// append the text you want
 	fout << "That ra to thich cau\n";
 	fout << "Tu rat lau roi\n";
 	fout.close();
 	cout << "Successful process\n";	
+}
+
+void convertTextToBinary()
+{
+	char* text = new char[50];
+	char* binary = new char[50];
+	cin.ignore();
+	cout << "Input text file name: ";
+	cin.getline(text, 51);
+	cout << "Input binary file name: ";
+	cin.getline(binary, 51);
+	ifstream fin;
+	fin.open(text, ios::in);
+	ofstream fout;
+	fout.open(binary, ios::out|ios::binary);
+	if (!fin.is_open())
+	{
+		cout << "Error when opening your file\n";
+		return;
+	}
+	while (!fin.eof())
+	{
+		int c;
+		fin >> c;
+		fout.write((char*)&c, sizeof(c));
+	}
+	
+	fin.close();
+	fout.close();	
+}
+
+void convertBinaryToText()
+{
+	
 }
 
 void Menu()
@@ -54,9 +88,11 @@ void Menu()
 	do 
 	{
 		cout << "\nProgram Handle File\n";
-		cout << "1. Writing file\n";
+		cout << "1. Writing a new file\n";
 		cout << "2. Writing more text to file\n";
 		cout << "3. Reading file\n";
+		cout << "4. Convert text to binary file\n";
+		cout << "5. Convert binary to text file\n";
 		cout << "0. Exit program\n";
 		cout << "----------------------\n";
 		cout << "What is your option?\n";
@@ -70,6 +106,8 @@ void Menu()
 			writing_file();
 		if (choice == 2)
 			writing_extra();
+		if (choice == 4)
+			convertTextToBinary();
 	} while (choice!=0);
 }
 
